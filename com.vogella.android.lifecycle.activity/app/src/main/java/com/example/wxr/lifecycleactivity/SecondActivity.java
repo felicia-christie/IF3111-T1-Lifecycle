0edf5535275bb3bbe869f2668318d1f29813d1eb
felicia-christie <waxrain1217@gmail.com>
2015-02-05 12:27:30 +0700

    Used button for calling second activity

---
+++ com.vogella.android.lifecycle.activity/app/src/main/java/com/example/wxr/lifecycleactivity/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.wxr.lifecycleactivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+/**
+ * Created by SPM on 05/02/2015.
+ */
+public class SecondActivity extends TracerActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
